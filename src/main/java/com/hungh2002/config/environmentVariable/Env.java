c20186be808c862ff084e407284443684dca2e87
hungh2002 <manhhung.h2002@gmail.com>
2024-05-11 14:49:15 +0900

    fix test file

--- src/main/java/com/hungh2002/config/environmentVariable/Env.java
+++ src/main/java/com/hungh2002/config/environmentVariable/Env.java
@@ -7,7 +7,7 @@ public class Env {
     public static final String RDBMS = "mysql";
     public static final String HOST = "localhost";
     public static final String PORT_OF_HOST = "3306";
-    public static final String DATABASE_NAME = "demo-online-shopping-website";
+    public static final String DATABASE_NAME = "demo_online_shopping_website";
     public static final String USERNAME = "hungh2002";
     public static final String PASSWORD = "1905";
 }
